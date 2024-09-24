1c5ffbc0b0479e29780fe6e4b2ac726809296649
raghavy11 <148036800+raghavy11@users.noreply.github.com>
2024-09-24 12:52:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var tl = gsap.timeline()
+
+tl.from("#nav h3" ,{
+    y:-50,
+    opacity:0,
+    delay:0.3,
+    duration:0.7,
+    stagger:0.3
+})
+
+tl.from("#main h1" ,{
+    x:-500,
+    opacity:0,
+    delay:0.3,
+    duration:0.7,
+    stagger:0.3
+})
+
+tl.from("img",{
+x:100,
+rotate:45,
+opacity:0,
+duration:0.5,
+stagger:0.5
+})
+
+ 
